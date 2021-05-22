c4b7b22b5f0dc6cf80bb9a2946d87681ef01d5ae
Sumit <Sumit@LAPTOP-6J972249>
2021-05-22 17:16:57 +0530

    other

--- java_core/src/main/java/java_core/Prime.java
+++ java_core/src/main/java/java_core/Prime.java
@@ -3,8 +3,26 @@ package java_core;
 public class Prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int num = sc.nextInt();
+		boolean flag = false;
+		for (int i = 2; i <= num / 2; i++) {
+			if(num%i == 0) {
+				flag = true;
+			}
+		}
+		
+		if(flag) {
+			System.out.println("Not a Prime Number .");
+		}
+		else {
+			System.out.println("This is a Prime Number !");
+		}
+		
+		
+		
+		sc.close();
 	}
 
 }
